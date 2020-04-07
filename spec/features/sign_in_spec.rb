efb1ca58b2553a03c6ca76e873cf95f5e4c83344
darkelv <darkelv777@yandex.ru>
2020-04-07 13:47:10 +0300

    Второй блок историй

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -6,23 +6,60 @@ feature 'User can sign in', %q(
   I'd like to be able to sign in
 ) do
 
-  given(:user) { create (:user) }
-
   background { visit new_user_session_path }
 
-  scenario 'Registered user tries to sign in' do
-    # visit login page
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
+  describe 'Registered user' do
+    given(:user) { create (:user) }
 
-    expect(page).to have_content 'Signed in successfully.'
+    scenario 'tries to sign in' do
+      # visit login page
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_on 'Log in'
+
+      expect(page).to have_content 'Signed in successfully.'
+    end
+
+    scenario 'tries to signet out' do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_on 'Log in'
+
+      click_on 'Log out'
+
+      expect(page).to have_content 'Signed out successfully'
+    end
+
+    scenario 'tries to sign up' do
+      click_on 'Sign up'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      fill_in 'Password confirmation', with: user.password
+      click_on 'Sign up'
+
+      expect(page).to have_content "Email has already been taken"
+    end
   end
-  scenario 'Unregistered user tries to sign in' do
-    fill_in 'Email', with: 'wrong@user.ru'
-    fill_in 'Password', with: '12345678'
-    click_on 'Log in'
 
-    expect(page).to have_content 'Invalid Email or password.'
+  describe "Unregistered user" do
+    scenario 'tries to sign in' do
+
+      fill_in 'Email', with: 'wrong@user.ru'
+      fill_in 'Password', with: '12345678'
+      click_on 'Log in'
+
+      expect(page).to have_content 'Invalid Email or password.'
+    end
+
+    scenario 'tries to sign up' do
+
+      click_on 'Sign up'
+      fill_in 'Email', with: 'wrong@user.ru'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_on 'Sign up'
+
+      expect(page).to have_content "Welcome! You have signed up successfully."
+    end
   end
 end
