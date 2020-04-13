63881b5b758f8d5ba2be6ae717b9e218917517b2
darkelv <darkelv777@yandex.ru>
2020-04-13 11:54:02 +0300

    add_question_file

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   has_many :answers, -> { order(best: :desc, created_at: :asc) }, dependent: :destroy
   belongs_to :user
+  has_one_attached :file
 
   has_many_attached :files
 
