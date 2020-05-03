64d0b8fe8116a7b7d5a61f56075da4f1de5cfa68
darkelv <darkelv777@yandex.ru>
2020-05-03 07:55:40 +0300

    first_api

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,14 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :profiles, only: [] do
+        get :me, on: :collection
+      end
+    end
+  end
+
   resources :attachments, only: :destroy
   resources :links, only: :destroy
   resources :awards, only: :index
